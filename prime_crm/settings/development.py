e54a5dee6dbdad5c322fc631b35da32720f3de97
Saad <saadhassan817@gmail.com>
2026-03-08 23:45:19 +0500

    CORS config

--- prime_crm/settings/development.py
+++ prime_crm/settings/development.py
@@ -9,19 +9,19 @@ SECRET_KEY = 'django-insecure-0#r^1yas045z^_j0e)o*x2q^%kmy(3t@j1g-a1yanm1-r)83(t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['muskox-dynamic-mutually.ngrok-free.app', 'localhost', '127.0.0.1', 'enclosure-settle-authentication-balance.trycloudflare.com']
+ALLOWED_HOSTS = ['muskox-dynamic-mutually.ngrok-free.app', 'localhost', '127.0.0.1', 'prime-crm-fe.vercel.app']
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000', 
     'https://muskox-dynamic-mutually.ngrok-free.app',
-    'https://enclosure-settle-authentication-balance.trycloudflare.com',
+    'https://prime-crm-fe.vercel.app'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 
     'https://muskox-dynamic-mutually.ngrok-free.app',
-    'https://enclosure-settle-authentication-balance.trycloudflare.com',
+    'https://prime-crm-fe.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
