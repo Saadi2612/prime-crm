abaea71249ab2b60bdd6f906fb079ee776129c40
Saad <saadhassan817@gmail.com>
2026-03-29 13:32:56 +0500

    ec2 ip add

--- prime_crm/settings/development.py
+++ prime_crm/settings/development.py
@@ -9,13 +9,14 @@ SECRET_KEY = 'django-insecure-0#r^1yas045z^_j0e)o*x2q^%kmy(3t@j1g-a1yanm1-r)83(t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['muskox-dynamic-mutually.ngrok-free.app', 'localhost', '127.0.0.1', 'prime-crm-fe.vercel.app', 'prime-crm-production-88.up.railway.app']
+ALLOWED_HOSTS = ['muskox-dynamic-mutually.ngrok-free.app', 'localhost', '127.0.0.1', 'prime-crm-fe.vercel.app', 'prime-crm-production-88.up.railway.app', '13.60.50.218']
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000', 
     'https://muskox-dynamic-mutually.ngrok-free.app',
     'https://prime-crm-fe.vercel.app',
-    'https://prime-crm-production-88.up.railway.app'
+    'https://prime-crm-production-88.up.railway.app',
+    'http://13.60.50.218:8000'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
@@ -23,7 +24,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000', 
     'https://muskox-dynamic-mutually.ngrok-free.app',
     'https://prime-crm-fe.vercel.app',
-    'https://prime-crm-production-88.up.railway.app'
+    'https://prime-crm-production-88.up.railway.app',
+    'http://13.60.50.218:8000'
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
